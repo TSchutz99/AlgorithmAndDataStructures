e8323ad86f4633bbb98928c165569cf8c7f1d05c
Faun <Faun.schuetz@gmail.com>
2021-02-11 16:58:10 +0000

    Commiting.

---
+++ Lab3/Encrypter.java
@@ -0,0 +1,54 @@
+package Lab3;
+
+import javax.swing.*;
+
+public class Encrypter {
+    public static String encrypterCaesarCipher(String plaintext, int offset){
+        StringBuilder cipherText = new StringBuilder("");
+        plaintext = plaintext.toUpperCase().replace(" ","");
+
+        if(offset >= 26){
+            offset = Integer.parseInt(JOptionPane.showInputDialog("Offset must be less then 26, Please reenter:"));
+        }
+
+        char ch;
+        for(int i = 0; i < plaintext.length(); i++)
+        {
+            ch = plaintext.charAt(i);
+            ch = (char)(ch + offset);
+
+            if(ch > 'Z'){
+                ch = (char)(ch - 'Z' + 'A' - 1);
+            }
+
+            cipherText.append(ch);
+        }
+
+        return cipherText.toString().replaceAll("(.{" + 5 + "})", "$1 ").trim();
+    }
+
+    public static String decryptRowColumn(String cipherText, int offset){
+        StringBuilder plainText = new StringBuilder("");
+        cipherText = cipherText.replace(" ", "");
+
+        if(offset >= 26){
+            offset = Integer.parseInt(JOptionPane.showInputDialog("Offset must be less then 26, Please reenter:"));
+        }
+
+        char ch;
+        for(int i = 0; i < cipherText.length(); i++)
+        {
+            ch = cipherText.charAt(i);
+            ch = (char)(ch - offset);
+
+            if(ch < 'A'){
+                ch = (char)(ch + 'Z' - 'A' + 1);
+            }
+
+            plainText.append(ch);
+        }
+
+
+        return plainText.toString();
+    }
+}
---
+++ Lab3/TestEncrypter.java
@@ -0,0 +1,19 @@
+package Lab3;
+
+import javax.swing.*;
+
+public class TestEncrypter {
+    public static void main(String[] args) {
+        // Input used for test purpose 'This is a secret Message dude'
+        String plainText = JOptionPane.showInputDialog("Please input some text to be encrypted:");
+        int offset = Integer.parseInt(JOptionPane.showInputDialog("Please input a number:"));
+
+        String cipherText = Encrypter.encrypterCaesarCipher(plainText, offset);
+
+        JOptionPane.showMessageDialog(null, cipherText, "Encrypted Text",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showMessageDialog(null, Encrypter.decryptRowColumn(cipherText, offset),
+                "Decrypted Text", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
